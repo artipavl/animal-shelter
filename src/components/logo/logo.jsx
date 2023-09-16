ceada91bbe500962bb30bd5c19dc3f7deb0dccc8
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-16 18:13:11 +0300

    create Logo

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import css from './logo.module.css';
+import logo from 'assets/Logo.png';
+
+const Logo = ({ variable = false }) => {
+  return (
+    <a className={css.Logo} href="/">
+      <img className={css.LogoImg} src={logo} alt="logo" />
+      <div className={variable && css.LogoTitleBox}>
+        <p className={css.LogoTitle}>ХАТИНА</p>
+        <p className={css.LogoSubTitle}>притулок для тварин</p>
+      </div>
+    </a>
+  );
+};
+
+export default Logo;
