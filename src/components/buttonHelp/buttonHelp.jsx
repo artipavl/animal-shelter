10c3fc5f3b65251c03964de91faa6842d9191c02
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-16 18:13:33 +0300

    create ButtonHelp

---
+++ src/components/buttonHelp/buttonHelp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './buttonHelp.module.css';
+
+const ButtonHelp = () => {
+  return (
+    <button className={css.ButtonHelp} type="button">
+      Допомогти
+    </button>
+  );
+};
+
+export default ButtonHelp;
