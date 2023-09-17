90f20905ea64d3a2e7a5bd65bd7e8d28687d1d49
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-17 18:39:34 +0300

    fix

--- src/components/language/language.jsx
+++ src/components/language/language.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import css from './language.module.scss';
 
 const Language = () => {
+  const [language, setLanguage] = useState('UA');
   return (
     <div className={css.LanguageBox}>
-      <select name="select" className={css.Language}>
+      <select
+        name="select"
+        className={css.Language}
+        value={language}
+        onChange={e => setLanguage(e.target.value)}
+      >
         <option value="UA">UA</option>
-        <option value="EN" selected>
-          EN
-        </option>
+        <option value="EN">EN</option>
       </select>
     </div>
   );
